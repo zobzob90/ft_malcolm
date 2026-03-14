ec1f1455767e13f678a394bd242620f0a892b0b7
Eric <eric.trignau@gmail.com>
2026-03-14 11:17:11 +0100

    structure and init done

---
+++ inc/malcolm.h
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malcolm.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 09:38:08 by eric              #+#    #+#             */
+/*   Updated: 2026/03/14 11:16:38 by eric             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MALCOLM_H
+#define MALCOLM_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if_arp.h>
+#include <net/if_packet.h>
+#include <netinet/if_ether.h>
+#include <arpa/inet.h>
+
+#include <time.h>
+
+#include "../libft/libft.h"
+
+#define ARP_REQUEST 1
+#define ARP_REPLY	2
+
+/*Structure header Ethernet, qui est le debut de tous les paquets reseaux LAN*/
+typedef struct s_ethernet
+{
+	uint8_t		dst_mac[6]; // -> 6 bytes, MAC du destinataire	AA:BB:CC:DD:EE:FF
+	uint8_t		src_mac[6]; // -> 6 bytes, MAC de l'emeteur 	11:22:33:44:55:66
+	uint16_t	type;		// -> 2 bytes, 0x0800 = IPv4, 0x0806 = ARP, 0x86DD = IPv6
+} __attribute__((packed)) t_ethernet;
+
+/*Structure ARP pour le contenu du paquet Address Rsolution Protocol*/
+typedef struct s_arp
+{
+	uint16_t	htype;			// -> type de reseau		(Ethernet)
+	uint16_t	ptype;			// -> protocole du reseau	(IPv4)
+	uint8_t		hlen;			// -> taille de l'adresse hardware
+	uint8_t		plen;			// -> taille de l'adresse protocole
+	uint16_t	opcode;			// -> type de paquet ARP (request/reply)
+	uint8_t		sender_mac[6];	// -> mac de l'emetteur (11:22:33:44:55:66)
+	uint8_t		sender_ip[4];	// -> ip de l'emetteur	(192.168.1.1)
+	uint8_t		target_mac[6];	// -> mac vise
+	uint8_t		target_ip[4];	// -> ip vise
+} __attribute__((packed)) t_arp;
+
+/*Sert a memoriser les associations IP->MAC en liste chainee pour stocker plusieurs machines*/
+typedef struct s_entry
+{
+	uint8_t			ip[4];
+	uint8_t			mac[6];
+	struct s_entry	*next;
+} t_entry;
+
+//init
+t_entry *add_entry(t_entry **table, uint8_t *ip, uint8_t *mac);
+
+// utils
+void	free_entry(t_entry *entry);
+void	print_mac(uint8_t mac[6]);
+void	print_ip(uint8_t ip[4]);
+
+#endif
--- src/init.c
+++ src/init.c
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/14 09:36:59 by eric              #+#    #+#             */
-/*   Updated: 2026/03/14 09:37:08 by eric             ###   ########.fr       */
+/*   Created: 2026/03/14 10:49:13 by eric              #+#    #+#             */
+/*   Updated: 2026/03/14 10:54:57 by eric             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "malcolm.h"
+
+t_entry *add_entry(t_entry **table, uint8_t *ip, uint8_t *mac)
+{
+	t_entry *new;
+
+	new = malloc(sizeof(t_entry));
+	if (!new)
+		return (NULL);
+	ft_memcpy(new->ip, ip, 4);
+	ft_memcpy(new->mac, mac, 6);
+	new->next = *table;
+	*table = new;
+	return (new);
+}
